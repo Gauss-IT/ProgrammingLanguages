3edcf5ec2622593c29848d6b46d1ce25930f741c
shkelqim627 <58366642+shkelqim627@users.noreply.github.com>
2020-06-24 06:45:10 +0200

    changes to index.html

--- Norway/script.js
+++ Norway/script.js
@@ -1,3 +1,4 @@
+document.body.innnerhtml = <h1> Working with Javascript...</h1>; 
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,4 +26,22 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
